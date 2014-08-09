9a8013b75a72c22d047da1f52a00a5f83897f264
Shunsuke Ishikuro <m5181135@u-aizu.ac.jp>
2014-08-10 04:55:28 +0900

    add view

---
+++ public/javascripts/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('stockStatusModule', []);
+
+app.controller('updateCtrl', function($scope, socket) {
+  socket.on('push_op', function (data) {
+    op = data;
+  })
+});
+
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }
+  };
+});
+
+
