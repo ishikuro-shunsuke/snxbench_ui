a7be8de4be6150bde836f40f1844f54c253db5a5
Shunsuke Ishikuro <m5181135@u-aizu.ac.jp>
2014-08-10 06:16:42 +0900

    merge express

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var io = require('socket.io').listen(app);
 var bodyParser = require('body-parser');
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort;
@@ -11,70 +10,6 @@ var serialPort = new SerialPort('COM6', {
   baudrate: 9600,
   buffersize: 1
 }, false);
-
-// socket.io
-io.on('connection', function (socket) {
-  socket.on('push_op', function (op) {
-    console.log('socket:data sending: ' + op);
-  })
-}),
-
-// serialport
-serialPort.open(function() {
-  console.log('SerialPort:open');
-  serialPort.on('data', function(data) {
-    var raw  = data[0].toString(2).split("").reverse().join("");
-    var inst = data[0].toString(2).split("").splice(0,4);
-    inst.unshift("0000");
-    inst = inst.join("");
-    var op;
-    switch (inst) {
-      case '00000000':
-      op = 'ADD';
-      break;
-      case '00000001':
-      op = 'AND';
-      break;
-      case '00000011':
-      op = 'SLT';
-      break;
-      case '00000100':
-      op = 'NOT';
-      break;
-      case '00000110':
-      op = 'SR';
-      break;
-      case '00000111':
-      op = 'HLT';
-      break;
-      case '00001000':
-      op = 'LD';
-      break;
-      case '00001001':
-      op = 'ST';
-      break;
-      case '00001010':
-      op = 'LDA';
-      break;
-      case '00001011':
-      op = 'AHI';
-      break;
-      case '00001110':
-      op = 'BZ';
-      break;
-      case '00001111':
-      op = 'BAL';
-      break;
-    }
-    if (op) {
-      io.sockets.emit('push_op', { op: op });
-      console.log('SerialPort:data received: ' + op);
-    } else {
-      console.log('SerialPort:data received: ' + raw);
-    }
-  });
-});
-
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -125,6 +60,71 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
+var io = require('socket.io').listen(app);
+
+// socket.io
+io.on('connection', function (socket) {
+  // serialport
+  serialPort.open(function() {
+    console.log('SerialPort:open');
+    serialPort.on('data', function(data) {
+      var raw  = data[0].toString(2).split("").reverse().join("");
+      var inst = data[0].toString(2).split("").splice(0,4);
+      inst.unshift("0000");
+      inst = inst.join("");
+      var op;
+      switch (inst) {
+        case '00000000':
+          op = 'ADD';
+          break;
+        case '00000001':
+          op = 'AND';
+          break;
+        case '00000011':
+          op = 'SLT';
+          break;
+        case '00000100':
+          op = 'NOT';
+          break;
+        case '00000110':
+          op = 'SR';
+          break;
+        case '00000111':
+          op = 'HLT';
+          break;
+        case '00001000':
+          op = 'LD';
+          break;
+        case '00001001':
+          op = 'ST';
+          break;
+        case '00001010':
+          op = 'LDA';
+          break;
+        case '00001011':
+          op = 'AHI';
+          break;
+        case '00001110':
+          op = 'BZ';
+          break;
+        case '00001111':
+          op = 'BAL';
+          break;
+      }
+      if (op) {
+        io.sockets.emit('push_op', op);
+        console.log('SerialPort:data received: ' + op);
+      } else {
+        console.log('SerialPort:data received: ' + raw);
+      }
+    });
+  });
+
+  socket.on('push_op', function (op) {
+    console.log('socket:data sending: ' + op);
+  })
+}),
 
 
 module.exports = app;
